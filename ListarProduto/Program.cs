d824b6b9b1c3bdef6825f8c688c2b7677569c742
EMERSON DE JESUS CANDIDO <emerson.jcandido1@senacsp.edu.br>
2022-11-18 21:57:26 -0300

    nova ideia

--- ListarProduto/Program.cs
+++ ListarProduto/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
-using Listar_lib;
+using Listar_lib.Itens;
 
 
 namespace ListarProduto
@@ -10,41 +10,17 @@ namespace ListarProduto
     {
         static void Main(string[] args)
         {
-            string Chaveconexao = "Data Source=10.39.45.44;Initial Catalog=TI_Noite;User ID=Turma2022;Password=Turma2022@2022";
 
-            string p_Nome_Produto = "Poste do Jax";
+            Lista_itens oLista = new Lista_itens();
+            DataSet ListProuto = new DataSet();
 
-            DataSet loja = new DataSet();
-            try
+            ListProuto = oLista.List_LOL("LISTA_PRODUTO", "MAN");
+
+            foreach (DataRow linha in ListProuto.Tables[0].Rows)
             {
-                switch () { 
-                
-                
-                }
-                SqlConnection Conexao = new SqlConnection(Chaveconexao);
-                Conexao.Open();
-                string wQuery = $"select * from Produto where Nome_Produto = '{p_Nome_Produto}'";
-                SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
-                adapter.Fill(loja);
-                // Conexao.Open();
-
-
-           
-                foreach (DataRow linha in loja.Tables[0].Rows)
-                {
-                    Console.WriteLine(linha["Nome_Produto"]);
-                }
-
-
-
+                Console.WriteLine(linha["Nome_Produto"]);
             }
-            catch (Exception)
-            {
 
-
-
-                throw;
-            }
         }
     }
 }
--- Listar_lib/Lib/Lista_itens.cs
+++ Listar_lib/Lib/Lista_itens.cs
@@ -7,10 +7,10 @@ using System.Text;
 
 namespace Listar_lib.Itens
 {
-    class Lista_itens
+    public class Lista_itens
     {
-        string ChaveConexao = "Data Source=10.39.45.44;Initial Catalog=Senac2022;Persist Security Info=True;User ID=Turma2022;Password=Turma2022@2022";
-        public DataSet List_LOL(string p_nome)
+        string ChaveConexao = "Data Source=10.39.45.44;Initial Catalog=TI_NOITE;Persist Security Info=True;User ID=Turma2022;Password=Turma2022@2022";
+        public DataSet List_LOL(string tipo_pesquisa, string p_nome)
         {
             DataSet DataSetlol = new DataSet();
 
@@ -18,7 +18,42 @@ namespace Listar_lib.Itens
             {
                 SqlConnection Conexao = new SqlConnection(ChaveConexao);
                 Conexao.Open();
-                string wQuery = $"select * from Costumer_List where Name like'%{p_nome}%'";
+                string wQuery =
+                        $"SELECT p.id_produto " +
+                        $",p.id_produto_grupo " +
+                        $",p.Nome_produto " +
+                        $",p.Descricao " +
+                        $",p.Codigo_Interno " +
+                        $",p.Data_Cadastro " +
+                        $",p.Status " +
+                        $",g.id_produto Grupo_id_produto " +
+                        $", g.id_produto_grupo Grupo_id_produto_grupo " +
+                        $", g.Nome_produto Grupo_Nome_produto " +
+                        $", g.Descricao Grupo_Descricao " +
+                        $", g.Codigo_Interno Grupo_Codigo_Interno " +
+                        $", g.Data_Cadastro Grupo_Data_Cadastro " +
+                        $", p.Status " +
+                        $"FROM Produto p, Produto g " +
+                        $"where p.id_produto_grupo = p.id_produto ";
+
+                switch (tipo_pesquisa.ToUpper())
+                {
+                    case "PRODUTO":
+                        wQuery = wQuery + $" and p.Nome_produto = '{p_nome}'"; 
+                        break;
+                    case "GRUPO":
+                        wQuery = wQuery + $" and g.Nome_produto = '{p_nome}'";
+                        break;
+                    case "LISTA_PRODUTO":
+                        wQuery = wQuery + $" and p.Nome_produto like '%{p_nome}%'";
+                        break;
+                    case "LISTA_GRUPO":
+                        wQuery = wQuery + $" and g.Nome_produto like '%{p_nome}%'";
+                        break;
+                    default:
+                        break;
+                }
+
                 SqlDataAdapter adapter = new SqlDataAdapter(wQuery, Conexao);
                 adapter.Fill(DataSetlol);
                 Conexao.Close();
@@ -123,7 +158,7 @@ namespace Listar_lib.Itens
         private void add_cep(Loja loja)
         {
             DataSet DataSetPesquisa = new DataSet();
-            DataSetPesquisa = List_LOL(loja.idProduto);
+            //DataSetPesquisa = List_LOL(loja.idProduto);
 
             if (DataSetPesquisa.Tables[0].Rows.Count == 0)
             {
